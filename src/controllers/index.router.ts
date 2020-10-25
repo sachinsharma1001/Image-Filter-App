36b19c91a54245db19ad489c333050aa68127766
sachinsharma1001 <sachin.sharma100197@gmail.com>
2020-10-25 19:10:46 +0530

    Implemented code for image-filter and sending file as a response

---
+++ src/controllers/index.router.ts
@@ -0,0 +1,34 @@
+import { Request, Response, Router } from "express"
+import { deleteLocalFiles, filterImageFromURL } from "../util/util";
+
+
+const router: Router = Router();
+
+router.get("/filteredimage", async (req: Request, res: Response) => {
+    let image_url = req.query.image_url;
+
+    if(!image_url) {
+        return res.status(400).send({message: "Invalid url. Please provide correct url"});
+    }
+
+    let filteredImagePath: string;
+    try {
+        filteredImagePath = await filterImageFromURL(image_url);
+    } catch(error) {
+        return res.status(409).send({message: "Error in filtering image"});
+    }
+
+    res.download(filteredImagePath, async (error) => {
+        if (error) {
+          res.status(204).end();
+        }
+    
+        try {
+          await deleteLocalFiles([filteredImagePath]);
+        } catch (err) {
+          
+        }
+      });
+});
+
+export const IndexRouter: Router = router;
